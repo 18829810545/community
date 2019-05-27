0240d4806191cf23b81b5e39f546433503b3a2e7
WIN10 <1429795441@qq.com>
2019-05-27 19:18:51 +0800

    frist commit

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
    @GetMapping("/hello")
     public String hello(@RequestParam(name="name") String name, Model model){
        model.addAttribute("name", name);
-       System.out.println("eeeeeeeee");
+       System.out.println("rrrrrrrgit ");
       return "hello";
    }
 }
